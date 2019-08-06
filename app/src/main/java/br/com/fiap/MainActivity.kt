67b0431c2546f014926388eb7a81dcda221a892d
Logon Aluno <logonaluno@local.com>
2019-08-05 23:00:18 -0300

    Criado os metodos para resultado activity.

--- app/src/main/java/br/com/fiap/MainActivity.kt
+++ app/src/main/java/br/com/fiap/MainActivity.kt
@@ -1,12 +1,29 @@
 package br.com.fiap
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnCalcular.setOnClickListener {
+            val proximaTela = Intent(this, ResultadoActivity::class.java)
+            proximaTela.putExtra("PESO", edtPeso.text.toString())
+            proximaTela.putExtra("ALTURA", edtAltura.text.toString())
+            proximaTela.putExtra("SEXO", spnSexo.selectedItem.toString())
+            startActivity(proximaTela)
+        }
+
+        btnLimpar.setOnClickListener {
+            edtPeso.setText("")
+            edtAltura.setText("")
+            spnSexo.setSelection(0)
+        }
+
     }
 }
--- app/src/main/java/br/com/fiap/ResultadoActivity.kt
+++ app/src/main/java/br/com/fiap/ResultadoActivity.kt
@@ -1,12 +1,77 @@
 package br.com.fiap
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_resultado.*
+import java.text.DecimalFormat
+import java.text.NumberFormat
 
 class ResultadoActivity : AppCompatActivity() {
+
+    var peso = 0.0
+    var altura = 0.0
+    var sexo = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_resultado)
+
+
+        carregaDados()
+        calcular()
+
     }
+
+    @SuppressLint("SetTextI18n")
+    private fun calcular() {
+        val imc = peso / (altura * altura)
+//        val dec = DecimalFormat("#,###.#")
+
+
+//        txvResultado.text = dec.format(imc)
+        txvResultado.text = imc.toFixed(1)
+
+
+        when {
+            imc < 18.5 -> {
+                txvMensagemResultado.text = getString(R.string.abaixo_peso)
+                setaImagem(R.drawable.fem_abaixo)
+            }
+            imc < 24.9 -> {
+                txvMensagemResultado.text = "Peso ideal"
+                setaImagem(R.drawable.fem_ideal)
+            }
+            imc < 29.9 -> {
+                txvMensagemResultado.text = "Levemente acima do peso"
+                setaImagem(R.drawable.fem_sobre)
+            }
+            imc < 34.9 -> {
+                txvMensagemResultado.text = "Obeso"
+                setaImagem(R.drawable.fem_obeso)
+            }
+            else -> {
+                txvMensagemResultado.text = "Extremamente Obeso"
+                setaImagem(R.drawable.fem_extremo_obeso)
+            }
+        }
+
+
+    }
+
+    fun setaImagem(resourceId: Int) {
+        imvResultado.setImageDrawable(
+            ContextCompat.getDrawable(this,
+                resourceId))
+    }
+
+
+    private fun carregaDados() {
+        peso = intent.getStringExtra("PESO").toDouble()
+        altura = intent.getStringExtra("ALTURA").toDouble()
+        sexo = intent.getStringExtra("SEXO")
+    }
+
+
 }
